5035333418ced6d0515b5d8ae5271767b5208bc3
YmClash <momo.toure27@gmail.com>
2024-01-20 05:42:33 +0100

    some change for final push

--- dr_vegapunk_lite.py
+++ dr_vegapunk_lite.py
@@ -17,15 +17,17 @@ intents.message_content = True
 
 bot = discord.Client(intents=intents)
 model = "gpt-4"
+model_Img = "dall-e-3"
 
-# on cree les deux fonction pour  1: generative  2: Generation d'image
+# on cree les deux fonction asynchrone pour  1: generative  2: Generation d'image
 async def demande_gpt(prompt) :
-    response = openai.ChatCompletion.create(
+    response = openai.chat.completions.create(
         model=model,
         messages=[
             {"role" : "system", "content" : "Je suis le Dr Vegapunk"},
             {"role" : "user", "content" : prompt}
         ],
+        # quelque parametre pour  mieux controle  le bot
         max_tokens=500,
         temperature=0.75,
         top_p=1.0,
@@ -34,5 +36,47 @@ async def demande_gpt(prompt) :
     )
     message = response.choices[0].message.content.strip()
     return message
+
+async def demande_image(prompt):
+    response = openai.images.generate(
+        model= model_Img,
+        prompt = prompt,
+        size="1024x1024",
+        quality= "standard",
+        n = 1,
+    )
+    # image_url =response['data'][0]['url']
+    image_url = response.data[0].url
+    return image_url
+
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in #momo-jam-cava as : {bot.user} , SUPER!!!!!!!!!!!!!!!!!  dev by Y_mC ')
+
+
+
+#  on vas utiliser un decorateur pour cree des command
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    #addresse de la chaine discord ou
+    dr_vegapunk_channel: discord.TextChannel = bot.get_channel(1099103151572926477)
+
+    if message.content.startswith("!"):
+        prompt = message.content[11:]
+        response = await demande_gpt(prompt)
+        await dr_vegapunk_channel.send(content=response)
+
+    if message.content.startswith("/img"):
+        prompt = message.content[11:]
+        response = await demande_image(prompt)
+        await dr_vegapunk_channel.send(content=response)
+
+
+bot.run(discord_api_key,log_level=logging.INFO)
+
+
 
 
